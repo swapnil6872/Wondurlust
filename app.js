5ed0abf595b428abeb7dc7ecd6b08588432af36f
Swapnil Gamit <gamitswapnil7@gmail.com>
2025-02-15 15:32:47 +0530

    created index route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose= require('mongoose');
 const Listing = require('./models/listing') 
+const path = require('path')
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wondurlust";
 
@@ -15,18 +16,31 @@ async function main(){
       await mongoose.connect (MONGO_URL)
 }
 
+app.set('view engine' ,'ejs');
+app.set('views',path.join(__dirname,'views'))
+
+
 app.get('/',(req,res)=>{
     res.send('hi i am root')
-})
-app.get('/test',async(req,res)=>{
-    let newList = await Listing.findOneAndUpdate({price:1200},{
+});
 
-        location:"goa",
-        country:"india"
-    });
-    await newList.save();
-    console.log('saved');
-    res.send('sucesss new')
+// app.get('/test',async(req,res)=>{
+//     let newList = await Listing.findOneAndUpdate({price:1200},{
+
+//         location:"goa",
+//         country:"india"
+//     });
+//     await newList.save();
+//     console.log('saved');
+//     res.send('sucesss new')
+// });
+
+
+
+// index Route
+app.get('/listings',async (req,res) =>{
+  const allListings = await Listing.find();
+  res.render('listings/index.ejs',({allListings}));
 })
 
 app.listen(8080,()=>{
--- init/index.js
+++ init/index.js
@@ -16,6 +16,7 @@ async function main(){
 
 const initDB = async ()=>{
    await Listing.deleteMany({});
+   await Listing.insertMany(initdata.data);
    console.log('data was initail')
 }
 
