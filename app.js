f9cf7bd93c7856f8d7df5183bd914871d3b237d9
Swapnil Gamit <gamitswapnil7@gmail.com>
2025-02-16 02:07:28 +0530

    created update&delete Route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose= require('mongoose');
 const Listing = require('./models/listing') 
 const path = require('path');
+const methodOverride = require('method-override')
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wondurlust";
@@ -20,6 +21,7 @@ async function main(){
 app.set('view engine' ,'ejs');
 app.set('views',path.join(__dirname,'views'))
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'))
 
 
 
@@ -68,6 +70,27 @@ app.post('/listings',async (req,res)=>{
      res.redirect('/listings');
     
 });
+
+//Edit Route
+app.get('/listings/:id/edit',async (req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render('listings/edit',{listing});
+});
+
+//update route
+app.put('/listings/:id', async (req,res)=>{
+      let {id} = req.params;
+      await Listing.findByIdAndUpdate(id,{...req.body.listings});
+      res.redirect('/listings/'+id)
+})
+
+//delete route
+app.delete('/listings/:id',async (req,res)=>{
+    let{id} = req.params;
+    let deleteListing = await Listing.findByIdAndDelete(id);
+    res.redirect('/listings');
+})
 
 app.listen(8080,()=>{
    console.log("server is listening to port 8080");
