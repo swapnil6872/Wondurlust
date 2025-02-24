4ad795792d0390ae5823d18843afd170a116d23a
Swapnil Gamit <gamitswapnil7@gmail.com>
2025-02-25 02:19:59 +0530

    implimented error flash includes

--- routes/listing.js
+++ routes/listing.js
@@ -30,6 +30,10 @@ router.get('/new', (req, res) => {
 router.get('/:id',wrapAsync( async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id).populate('reviews');;
+    if(!listing){
+        req.flash('error',"Listing you requested for does not exist!");
+        res.redirect('/listings')
+    }
     res.render('listings/show', { listing });
 }))
 
@@ -48,6 +52,10 @@ router.post('/', validateListing,wrapAsync(async(req,res,next) => {
 router.get('/:id/edit',wrapAsync( async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
+    if(!listing){
+        req.flash('error',"Listing you requested for does not exist!");
+        res.redirect('/listings')
+    }
     res.render('listings/edit', { listing });
 }));
 
