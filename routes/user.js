3092f15387411da3ebbd5ce9a8bd0ee5ce875fbe
Swapnil Gamit <gamitswapnil7@gmail.com>
2025-02-25 15:59:57 +0530

    created Login

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 const User = require('../models/user');
 const wrapAsync = require('../utils/wrapAsync');
+const passport = require('passport');
 
+//signup
 router.get('/signup', (req, res) => {
     res.render("users/signup");
 })
-
+//signup
 router.post('/signup', wrapAsync(async (req, res) => {
     try {
         let { username, email, password } = req.body;
@@ -15,11 +17,25 @@ router.post('/signup', wrapAsync(async (req, res) => {
         console.log(registerUser);
         req.flash('success', 'Welcome to wanderlust')
         res.redirect('/listings')
-    }catch(error){
-        req.flash('error',error.message);
+    } catch (error) {
+        req.flash('error', error.message);
         res.redirect('/signup')
     }
-   
+
 }))
+
+//login
+router.get('/login', (req, res) => {
+    res.render('users/login');
+})
+//login
+router.post('/login', passport.authenticate('local', {
+    failureRedirect: '/login',
+    failureFlash: true
+}),
+    async (req, res) => {
+        req.flash('success', 'welcome to wonderlust');
+        res.redirect('/listings')
+    })
 
 module.exports = router;
