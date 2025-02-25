3aba6ee059f22b69ca7a7b96ec21ff056a8addcc
Swapnil Gamit <gamitswapnil7@gmail.com>
2025-02-25 14:49:03 +0530

    configured LocalStrategy of Passport

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const ejsMate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressError');
 const session = require('express-session')
 const flash = require('connect-flash');
+const passport=require('passport');
+const LocalStrategy = require('passport-local');
+const User=require('./models/user');
 
 const listings = require('./routes/listing');
 const reviews =require('./routes/review')
@@ -49,6 +52,14 @@ app.get('/', (req, res) => {
 
 app.use(session(sessionOptions));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session()); 
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.use((req,res,next)=>{
     res.locals.success =req.flash('success');
