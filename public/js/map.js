0691825caa6a8413ac859315caca4b7bae893446
Swapnil Gamit <gamitswapnil7@gmail.com>
2025-03-05 23:06:53 +0530

    Implimented Map Marker

--- public/js/map.js
+++ public/js/map.js
@@ -2,9 +2,12 @@ mapboxgl.accessToken =mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [73.1812, 22.3072], // starting position [lng, lat]
+    center: coordinates, // starting position [lng, lat]
     zoom: 9, // starting zoom
 });
 
 
+const marker1 = new mapboxgl.Marker( {color: 'red'})
+        .setLngLat(coordinates) // listing.geometry.coordianates
+        .addTo(map);
 
